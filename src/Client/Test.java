e89e9c93ed8d8d5f12cc57fa70515fd83f66778f
AbdelhadiBERRAH <abdelhadi.berrah.algerie@viacesi.fr>
2013-01-28 15:55:36 +0100

    Update src/Client/Test.java

--- src/Client/Test.java
+++ src/Client/Test.java
@@ -6,17 +6,14 @@ import PrositDAO.*;
 
 public class Test {
 
-	/**
-	 * @param args
-	 * @throws SQLException 
-	 */
+
 	public static void main(String[] args) throws SQLException {
 		PrstDAO prst = new PrstDAO();
 		prst.listidPage();
 		prst.listidVisiteur();
 		prst.listtaille();
 		
-		// TODO Auto-generated method stub
+	
 
 	}
 
